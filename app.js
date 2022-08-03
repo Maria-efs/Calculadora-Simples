42b51ab471e966118781a302a1c71d5d32eb1d28
Maria-efs <mfs.6401131@gmail.com>
2022-08-03 15:07:22 -0300

    atualizado 03/08/2022

--- app.js
+++ app.js
@@ -1,10 +1,42 @@
 'use strict'
 
-const number1 = document.getElementById('number1').value;
-const number2 = document.getElementById('number2').value;
-const resultado = document.getElementById('resultado');
+function somar (){
+    const number1 = document.getElementById('number1').value;
+    const number2 = document.getElementById('number2').value;
+    const resultado = document.getElementById('resultado')
 
-function somar (number1, number2){
-    resultado = parseInt(number1) + parseInt(number2)
+    const valor = parseInt(number1) + parseInt(number2);
+    resultado.textContent = valor
 }
-document.getElementById('mais').addEventListener('click', somar);
+
+function subtrair (){
+    const number1 = document.getElementById('number1').value;
+    const number2 = document.getElementById('number2').value;
+    const resultado = document.getElementById('resultado')
+
+    const valor = parseInt(number1) - parseInt(number2);
+    resultado.textContent = valor
+}
+
+function multiplicar(){
+    const number1 = document.getElementById('number1').value;
+    const number2 = document.getElementById('number2').value;
+    const resultado = document.getElementById('resultado')
+
+    const valor = parseInt(number1) * parseInt(number2);
+    resultado.textContent = valor
+}
+
+function divisao (){
+    const number1 = document.getElementById('number1').value;
+    const number2 = document.getElementById('number2').value;
+    const resultado = document.getElementById('resultado')
+
+    const valor = parseInt(number1) / parseInt(number2);
+    resultado.textContent = valor
+}
+
+mais.addEventListener('click', somar)
+menos.addEventListener('click', subtrair)
+vezes.addEventListener('click', multiplicar)
+dividir.addEventListener('click', divisao)
